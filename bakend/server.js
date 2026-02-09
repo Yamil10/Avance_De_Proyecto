c5a07c28d39d0dfcf68835e214a8a80a2a6fa78f
Yamil28XD <yamil3307@gmail.com>
2026-02-09 11:46:56 -0600

    Update server.js

--- bakend/server.js
+++ bakend/server.js
@@ -80,4 +80,6 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000; 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
